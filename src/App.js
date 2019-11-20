645f67d6bac4613a1d676b55c76dda0f8c02a436
Kate Howard <k.l.howard.1992@gmail.com>
2019-11-20 21:46:19 +0000

    Start timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import useInterval from './hooks/useInterval';
 import { makepuzzle, solvepuzzle } from 'sudoku';
 import Cell from './components/Cell';
 import './App.css';
@@ -7,12 +8,22 @@ const puzzle = makepuzzle();
 const solution = solvepuzzle(puzzle).map(number => number + 1);
 
 function App() {
+  const [playing, togglePlaying] = useState(false);
+  const [time, setTime] = useState(0);
   const [guesses, setGuesses] = useState(puzzle.map(number => (number ? number + 1 : null)));
   const [status, setStatus] = useState(null);
+
+  useInterval(
+    () => {
+      setTime(time + 1);
+    },
+    playing ? 1000 : null
+  );
 
   useEffect(() => {
     if (guesses.join('') === solution.join('')) {
       setStatus('solved');
+      togglePlaying(false);
     } else if (guesses.every(guess => typeof guess === 'number')) {
       setStatus('filled');
     }
@@ -20,6 +31,7 @@ function App() {
 
   return (
     <div>
+      <button onClick={() => togglePlaying(true)}>Play</button>
       <div className="grid">
         {puzzle.map((number, index) => (
           <Cell
@@ -39,6 +51,10 @@ function App() {
           />
         ))}
       </div>
+      <p>
+        {Math.floor(time / 60) < 10 ? `0${Math.floor(time / 60)}` : Math.floor(time / 60)}:
+        {time % 60 < 10 ? `0${time % 60}` : time % 60}
+      </p>
     </div>
   );
 }
