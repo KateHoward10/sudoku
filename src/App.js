56df8749027a80f423ca9d042e9194de5c79ff91
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-14 21:24:20 +0000

    Set top games in cookies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import useInterval from './hooks/useInterval';
 import { makepuzzle, solvepuzzle, ratepuzzle } from 'sudoku';
+import { formatTime, getRating } from './helpers';
+import { useCookies } from 'react-cookie';
 import Controls from './components/Controls';
 import Cell from './components/Cell';
 import Grid from './components/Grid';
@@ -12,6 +14,8 @@ function App() {
   const [rating, setRating] = useState(null);
   const [guesses, setGuesses] = useState(null);
   const [status, setStatus] = useState(null);
+  const [cookies, setCookie] = useCookies(['topGames']);
+  const { topGames } = cookies;
 
   function start() {
     setPuzzle(Array.from(Array(81)).fill(null));
@@ -49,10 +53,14 @@ function App() {
     ) {
       setStatus('solved');
       togglePlaying(false);
+      if (!topGames.length || time < topGames[0].time) {
+        const updatedGames = [{ time, rating }, ...topGames];
+        setCookie('topGames', JSON.stringify(updatedGames));
+      }
     } else if (guesses && guesses.every(guess => typeof guess === 'number')) {
       setStatus('filled');
     }
-  }, [guesses, puzzle]);
+  }, [guesses, puzzle, topGames, time, rating, setCookie]);
 
   return (
     <div>
@@ -77,10 +85,18 @@ function App() {
           />
         ))}
       </Grid>
-      <p>
-        {Math.floor(time / 60) < 10 ? `0${Math.floor(time / 60)}` : Math.floor(time / 60)}:
-        {time % 60 < 10 ? `0${time % 60}` : time % 60}
-      </p>
+      <p>{formatTime(time)}</p>
+      <div>
+        <h3>Your top games:</h3>
+        <ul>
+          {topGames &&
+            topGames.map((game, index) => (
+              <li key={index}>
+                {formatTime(game.time)}: ({getRating(game.rating)})
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/helpers.js
+++ src/helpers.js
@@ -16,3 +16,9 @@ export function getRating(number) {
       return null;
   }
 }
+
+export function formatTime(time) {
+  return `${Math.floor(time / 60) < 10 ? `0${Math.floor(time / 60)}` : Math.floor(time / 60)}:${
+    time % 60 < 10 ? `0${time % 60}` : time % 60
+  }`;
+}
