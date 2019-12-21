07a1829310ecb369fd7315fafd5323cf2c6b3f8e
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-21 18:56:19 +0000

    Updates

---
+++ precache-manifest.f9ec9937e72a8e678c79c06cc0cfa9dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dda12ff2ffbe5a3f64fbd80bee747f",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "4748252e5989271e2592",
+    "url": "/sudoku/static/css/main.09285f9b.chunk.css"
+  },
+  {
+    "revision": "801668901fb3d0b788eb",
+    "url": "/sudoku/static/js/2.c97f7135.chunk.js"
+  },
+  {
+    "revision": "4748252e5989271e2592",
+    "url": "/sudoku/static/js/main.9c48433d.chunk.js"
+  },
+  {
+    "revision": "60a5e5439fa8d06f9652",
+    "url": "/sudoku/static/js/runtime-main.6c2b2ca1.js"
+  }
+]);
