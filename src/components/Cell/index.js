149fdc5a55b4a490f778e137644ec29df6dbc7b1
Kate Howard <k.l.howard.1992@gmail.com>
2019-12-23 17:36:09 +0000

    Set focus on click

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -9,7 +9,7 @@ function Cell({ index, number, onFocus, onEnter, value, wrong, currentInput }) {
       ) : (
         <React.Fragment>
           <Input value={value} onChange={onEnter} />
-          <Button onFocus={onFocus} focused={currentInput === index}>
+          <Button onClick={onFocus} focused={currentInput === index}>
             {value}
           </Button>
         </React.Fragment>
